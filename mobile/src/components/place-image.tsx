d8a28b1918a87e0ef9ae2d1e5e86fc7046154560
qet1234 <144639971+qet1234@users.noreply.github.com>
2026-08-10 14:16:56 +0900

    모바일 색상을 웹 디자인과 통일

--- mobile/src/components/place-image.tsx
+++ mobile/src/components/place-image.tsx
@@ -33,7 +33,7 @@ export function PlaceImage({
 }
 
 const styles = StyleSheet.create({
-  wrapper: { overflow: 'hidden', borderRadius: 14, backgroundColor: '#eef2ef' },
+  wrapper: { overflow: 'hidden', borderRadius: 14, backgroundColor: '#eee9dd' },
   image: { width: '100%', height: 160 },
   attribution: {
     position: 'absolute',
